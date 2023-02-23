f77e651f9b2d495efc4b0192614080a79c5a034c
Muhammad <muhjwd@gmail.com>
2023-02-23 02:40:10 -0800

    Task

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,12 @@
+/**
+ * _isdigit - checks if input is digit between 0 - 9
+ * Return: 1 if is digit, and 0 if not
+*/
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+		return (1);
+	else
+		return (0);
+}
