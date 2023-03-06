3c39663ee4a29d7eecbd77e2b25e8d3677f606eb
Muhammad <muhjwd@gmail.com>
2023-03-06 11:40:03 -0800

    Task

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -2,10 +2,9 @@
 /**
  * set_string - sets the value of a pointer to a char
  * @s: pointer to pointer
- * @to: pointer to char
- * Return: void
+ * @to: pointer char
  */
 void set_string(char **s, char *to)
 {
-	*s = to;
+        *s = to;
 }
