8e1123b5f8d2330bfa198226185951007a141ae0
Muhammad <muhjwd@gmail.com>
2023-03-08 15:13:41 -0800

    Task

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,59 +1,50 @@
 #include "main.h"
 
-int find_strlen(char *s);
-int check_palindrome(char *s, int len, int index);
-int is_palindrome(char *s);
-
 /**
- * find_strlen - Returns the length of a string
- * @s: The string to be measured
- * Return: The length of the string.
+ * _strlen_recursion - returns the length of a string
+ * @s: the string to measure
+ *
+ * Return: the length of a string
  */
-int find_strlen(char *s)
+
+int _strlen_recursion(char *s)
 {
-	int len = 0;
-
-	if (*(s + len))
-	{
-		len++;
-		len += find_strlen(s + len);
-	}
-
-	return (len);
+	if (*s == '\0')
+		return (0);
+	return (_strlen_recursion(s + 1) + 1);
 }
 
 /**
- * check_palindrome - Checks if a string is a palindrome.
- * @s: The string to be checked
- * @len: The length of s
- * @index: The index of the string to be checked
- * Return: If the string is a palindrome is 1
- *         If the string is not a palindrome is 0.
+ * is_pal_check - checks if the string is a palindrome
+ * @s: the string to test
+ * @end: the last index value of the string
+ * @i: the first index of the string
+ *
+ * Return: 1 if is pal, 0 if no
  */
-int check_palindrome(char *s, int len, int index)
+
+int is_pal_check(char *s, int end, int i)
 {
-	if (s[index] == s[len / 2])
+	if (s[end] == s[i] && (i == end - 1 || i == end))
 		return (1);
-
-	if (s[index] == s[len - index - 1])
-		return (check_palindrome(s, len, index + 1));
-
-	return (0);
+	if (s[end] != s[i])
+		return (0);
+	return (is_pal_check(s, end - 1, i + 1));
 }
 
+
 /**
- * is_palindrome - Checks if a string is a palindrome.
- * @s: The string to be checked
- * Return: If the string is a palindrome is 1.
- * If the string is not a palindrome is 0.
+ * is_palindrome - checks if the string is a palindrome
+ * @s: the string to test
+ *
+ * Return: 1 if palindrome, 0 if no
  */
+
 int is_palindrome(char *s)
 {
-	int index = 0;
-	int len = find_strlen(s);
+	int i = _strlen_recursion(s);
 
-	if (!(*s))
+	if (i <= 1)
 		return (1);
-
-	return (check_palindrome(s, len, index));
+	return (is_pal_check(s, i - 1, 0));
 }
