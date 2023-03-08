febd0957f74e117683e0e940a6d5ad47f434b458
Muhammad <muhjwd@gmail.com>
2023-03-08 15:02:30 -0800

    Task

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -4,7 +4,7 @@ int is_divisible(int num, int div);
 int is_prime_number(int n);
 
 /**
- * is_divisible -Function Checks if a number is divisible
+ * is_divisible - Function Checks if a number is divisible
  * @num: The number to be checked
  * @div: The divisor
  * Return: If the number is divisible - 0.
