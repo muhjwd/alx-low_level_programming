433bb23e04023bf097870524ec464863c13fb56c
Muhammad <muhjwd@gmail.com>
2023-03-08 15:12:18 -0800

    Task

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,22 +1,10 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-int _putchar(char c);
-void _puts_recursion(char *s);
-void _print_rev_recursion(char *s);
-int _strlen_recursion(char *s);
-int factorial(int n);
-int _pow_recursion(int x, int y);
-int find_sqrt(int num, int root);
-int _sqrt_recursion(int n);
-int is_divisible(int num, int div);
-int is_prime_number(int n);
-int find_strlen(char *s);
-int check_palindrome(char *s, int len, int index);
+int _putchar(char);
+void _puts_recursion(char *);
+void _print_rev_recursion(char *);
+int _strlen_recursion(char *);
+int factorial(int);
+int _pow_recursion(int, int);
+int _sqrt_recursion(int);
+int is_prime_number(int);
 int is_palindrome(char *s);
-int strlen_no_wilds(char *str);
-void iterate_wild(char **wildstr);
-char *postfix_match(char *str, char *postfix);
 int wildcmp(char *s1, char *s2);
-
-#endif /* MAIN_h */
