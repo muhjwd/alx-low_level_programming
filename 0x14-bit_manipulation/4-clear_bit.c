43c9a26125789620fd22b6b291df27227260e1ce
muhjwd <muhjwd@gmail.com>
2023-04-08 05:52:05 -0700

    Task

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -3,8 +3,8 @@
 /**
  * clear_bit - sets the value of a bit to 0 at given index
  *
- * @n: the number you want to set value to 0
- * @index: the index of the bit that you want
+ * @n: the number we want to set value to 0
+ * @index: the index of the bit that we want
  *
  * Return: 1 if success or -1 if an error occured
  */
